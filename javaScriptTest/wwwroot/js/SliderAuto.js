c2a63fcb3fdf3823be361033365fcc537fb5602f
YoonkunLee <ydx9474@aut.ac.nz>
2020-02-26 16:14:22 +1300

    .

--- javaScriptTest/wwwroot/js/SliderAuto.js
+++ javaScriptTest/wwwroot/js/SliderAuto.js
@@ -1,19 +1,19 @@
-﻿var slideIndex = 0;
-showSlides();
+﻿var slideIndex1 = 0;
+showSlides1();
 
-function showSlides() {
+function showSlides1() {
     var i;
-    var slides = document.getElementsByClassName("myAutoSlides");
-    var dots = document.getElementsByClassName("autoDot");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+    var slides1 = document.getElementsByClassName("myAutoSlides");
+    var dots1 = document.getElementsByClassName("autoDot");
+    for (i = 0; i < slides1.length; i++) {
+        slides1[i].style.display = "none";
     }
-    slideIndex++;
-    if (slideIndex > slides.length) { slideIndex = 1 }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
+    slideIndex1++;
+    if (slideIndex1 > slides1.length) { slideIndex1 = 1 }
+    for (i = 0; i < dots1.length; i++) {
+        dots1[i].className = dots1[i].className.replace(" active", "");
     }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-    setTimeout(showSlides, 2000); // Change image every 2 seconds
+    slides1[slideIndex1 - 1].style.display = "block";
+    dots1[slideIndex1 - 1].className += " active";
+    setTimeout(showSlides1, 2000); // Change image every 2 seconds
 }
